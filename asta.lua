195a656753bd79f7a76b1cadffe24291f88e22e6
asteniospaintos <134234448+asteniospaintos@users.noreply.github.com>
2023-05-22 13:43:15 -0300

    Update asta.lua

--- asta.lua
+++ asta.lua
@@ -3,14 +3,18 @@
 local UI = {}
 
 function UI.button(text, callback)
+    local aaaaa = Instance.new("ScreenGui")
+    aaaaa.Parent = game:WaitForChild("CoreGui")
+    
     local button = Instance.new("TextButton")
     button.Name = "Button"
     button.Text = text
     button.Size = UDim2.new(0, 110, 0, 35)
     button.Position = UDim2.new(0.5, -55, 0.5, -17.5)
+    button.AnchorPoint = Vector2.new(0.5, 0.5) -- Posiciona o botão em relação ao centro da tela
     button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     button.BorderSizePixel = 0
-    button.Parent = game.Players.LocalPlayer.PlayerGui
+    button.Parent = aaaaa
 
     local corner = Instance.new("UICorner")
     corner.CornerRadius = UDim.new(0, 8)
